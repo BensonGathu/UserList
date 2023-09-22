b2181b40bca738319b4c3007ea62b8960c129f52
Benson Gathu <bensongathu23@gmail.com>
2023-09-22 22:45:34 +0300

    userData provider added to handle state management

--- usersapp/lib/main.dart
+++ usersapp/lib/main.dart
@@ -1,10 +1,17 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:usersapp/providers/userDataProvider.dart';
 import 'package:usersapp/routes.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider<UserDataProvider>(
+        create: (context) => UserDataProvider())
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatefulWidget {
