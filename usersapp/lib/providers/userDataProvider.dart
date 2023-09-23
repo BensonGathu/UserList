9751346c157b85c3e4f221ae3243a41d8514f74f
Benson Gathu <bensongathu23@gmail.com>
2023-09-23 15:12:00 +0300

    Data displayed on the app

--- usersapp/lib/providers/userDataProvider.dart
+++ usersapp/lib/providers/userDataProvider.dart
@@ -7,6 +7,9 @@ import 'package:usersapp/services/api.dart';
 class UserDataProvider extends ChangeNotifier {
   List<UserModel> _allUsers = [];
   List<UserModel> get allUsers => _allUsers;
+  bool _isDataLoaded = false; 
+
+  bool get isDataLoaded => _isDataLoaded; 
 
   Future<void> fetchAllUsers() async {
     try {
@@ -16,11 +19,14 @@ class UserDataProvider extends ChangeNotifier {
         var userItems = json.decode(response.body);
         _allUsers = List<UserModel>.from(
             userItems.map((image) => UserModel.fromJson(image)));
+        _isDataLoaded = true; 
         notifyListeners();
       } else {
+        // Handle error here, such as displaying an error message or logging.
         print('Request failed with status: ${response.statusCode}');
       }
     } catch (e) {
+      // Handle exceptions that may occur during the API request.
       print('Error during API request: $e');
     }
   }
--- usersapp/lib/screens/homePage.dart
+++ usersapp/lib/screens/homePage.dart
@@ -1,21 +1,69 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:usersapp/providers/userDataProvider.dart';
 
 class HomePageScreen extends StatefulWidget {
-  const HomePageScreen({super.key});
+  const HomePageScreen({Key? key}) : super(key: key);
 
   @override
   State<HomePageScreen> createState() => _HomePageScreenState();
 }
 
 class _HomePageScreenState extends State<HomePageScreen> {
+  Future<void> _refreshData(BuildContext context) async {
+    final allUsersDataProvider =
+        Provider.of<UserDataProvider>(context, listen: false);
+    await allUsersDataProvider.fetchAllUsers();
+  }
+
   @override
   Widget build(BuildContext context) {
-    var allUsersDataProvider =
+    final ThemeData theme = Theme.of(context);
+    final bool isDarkMode = theme.brightness == Brightness.dark;
+
+    final allUsersDataProvider =
         Provider.of<UserDataProvider>(context, listen: false);
-        print(allUsersDataProvider.allUsers);
-    return const Placeholder();
+
+    return Scaffold(
+      // backgroundColor: isDarkMode ? Colors.black : Colors.white,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('User List'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () => _refreshData(context),
+        child: ListView.builder(
+          itemCount: allUsersDataProvider.allUsers.length,
+          itemBuilder: (context, index) {
+            final user = allUsersDataProvider.allUsers[index];
+            return Card(
+              margin: const EdgeInsets.all(10.0),
+              elevation: 5.0,
+              child: ListTile(
+                title: Text(
+                  user.name,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: isDarkMode
+                        ? Colors.white 
+                        : Colors.black,
+                  ),
+                ),
+                subtitle: Text(
+                  'Age: ${user.age.toString()}, Email: ${user.email}',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: isDarkMode
+                        ? Colors.grey 
+                        : Colors.black87,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
